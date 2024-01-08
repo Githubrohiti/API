b1844fc20e3005c36fe9b7e928a9d6e81cd69ca6
RohitKrishna <rksah37@gmail.com>
2024-01-08 02:43:09 +0000

    first commit

---
+++ src/com/helloword/GIT.java
@@ -0,0 +1,12 @@
+package com.helloword;
+
+public class GIT {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome1"); 
+		System.out.println("Welcome2");
+		System.out.println("Welcome3");
+	}
+
+}
